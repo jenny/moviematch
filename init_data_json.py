d4e8d8eec8e312f2ec86cfacecb37a25c2aa090e
Jenny Donnelly <39464+jenny@users.noreply.github.com>
2026-03-10 10:05:10 -0400

    Initial commit: Fetch data for top 100 movies from TMDB, create embeddings via local Sentence Transformers model, store embeddings to local ChromaDB, allow queries from CLI.

---
+++ init_data_json.py
@@ -0,0 +1,174 @@
+import os
+import requests
+import json
+import time # todo add sleep to tmdb calls
+import math
+
+from dotenv import load_dotenv
+from anthropic import Anthropic
+
+
+load_dotenv() # Reload the .env file
+CLAUDE_KEY = os.getenv("ANTHROPIC_API_KEY")
+TMDB_KEY = os.getenv("TMDB_READ_ACCESS_TOKEN")
+TMDB_BASE_URL = "https://api.themoviedb.org/3"
+TMDB_HEADERS = headers={
+    "accept": "application/json",
+    "Authorization": "Bearer " + TMDB_KEY
+} 
+
+# already initialized data for top 100 movies, don't need to do again
+def initialize_app_data(n):
+    top_movie_ids = initialize_index(n)
+    for movie in top_movie_ids:
+        initialize_movie_metadata(movie)
+    # todo: remove the for loop from the function below
+    # and use the for loop above
+    initialize_movie_richtext()
+
+
+def initialize_index(n):
+    pages = math.ceil(n/20)
+    ids = []
+    index = {"results": []}
+
+
+    with open("data/index.json", "w") as file:
+        for page in range (1,pages+1):
+            # print("page " + str(page))
+            response = requests.get(
+                TMDB_BASE_URL + "/movie/top_rated",
+                params = {
+                    "page": str(page) 
+                },
+                headers = TMDB_HEADERS
+            )
+            for movie in response.json()["results"]:
+                if len(ids) < n: 
+                    ids += [movie["id"]]
+                    # print(str(len(ids)) + " " + str(movie["id"]) + " " + movie["title"])
+    
+                    # write an index of all fetched ids to file
+                    index["results"].append({"id": movie["id"], "title": movie["title"]})
+                    
+        json.dump(index, file, indent=2)
+        print("Dumped indexes for " + str(len(index["results"])) + " movies to data/index.json.")
+    return ids
+
+
+def initialize_movie_metadata(movie_id):
+    response = requests.get(
+        TMDB_BASE_URL + "/movie/"+str(movie_id)+"?append_to_response=keywords,credits",
+        headers = TMDB_HEADERS
+    )
+    movie_json = response.json()
+    
+    # filter cast here
+    movie_json = filter_movie_cast(movie_json)
+
+    # filter crew here
+    movie_json = filter_and_sort_movie_crew(movie_json)
+    
+    with open("data/" + str(movie_id) + ".json", "w") as file:
+        json.dump(movie_json, file, indent=2)
+        print("Dumped filtered movie metadata for " + str(movie_json["title"]) + " to " + str(movie_id) + ".json")
+
+def filter_movie_cast(movie_json):
+    cast = movie_json["credits"]["cast"]
+    cast = cast[0:30]  # only keep top 30 billed actors
+    movie_json["credits"]["cast"] = cast
+    return movie_json
+
+
+def filter_and_sort_movie_crew(movie_json):
+    crew = movie_json["credits"]["crew"]
+    filtered_crew = []
+    for member in crew:
+        if (member["job"] == "Director" or
+            member["job"] == "Executive Producer" or
+            member["job"] == "Producer"):
+            filtered_crew.append(member)
+    sorted_crew = sorted(filtered_crew, key = lambda c: c.job)
+    movie_json["credits"]["crew"] = filtered_crew    
+    return movie_json
+
+# Compiles a 'richtext' string for each movie: 
+# title + plot summary + genre names + top 5 keywords into a single string
+# and inserts into each movies's json file
+def initialize_movie_richtext():
+    with open("data/index.json", "r") as index:
+        
+        movies = json.load(index)
+        
+        for movie in movies["results"]:
+            file_name = "data/" + str(movie["id"]) + ".json"
+            movie_json = {}
+            rich_text = ""
+
+            with open(file_name, "r") as file:
+                movie_json = json.load(file)
+
+                rich_text = "Title: " + movie_json["title"] + " (" + movie_json["release_date"][:4] + ")\n\n"
+                for crew in movie_json["credits"]["crew"]:
+                    rich_text += crew["job"] + ": " + crew["name"] + "\n\n"
+                rich_text += "Genres: "
+                rich_text += ', '.join(g["name"] for g in movie_json.get("genres", []))
+                rich_text += "\n\n"
+                rich_text += "Themes and Keywords: "
+                rich_text += ', '.join(k["name"] for k in movie_json["keywords"].get("keywords", []))
+                rich_text += "\n\n"
+                rich_text += "Top Cast: "
+                rich_text += ', '.join(c["name"] for c in movie_json["credits"].get("cast", []))
+                rich_text += "\n\n"
+                rich_text += "Plot: " + movie_json["overview"] + "\n\n"
+
+                movie_json["richtext"] = rich_text
+            
+            with open(file_name, "w") as file:
+                json.dump(movie_json, file, indent=2)
+                print("Dumped rich text for " + str(movie_json["title"]) + " to " + str(movie_json["id"]) + ".json")
+
+
+# richtext word count
+def debug_wordcount(field):
+    print("Wordcounts for " + field)
+    with open("data/index.json", "r") as index:
+            
+            movies = json.load(index)
+            
+            for movie in movies["results"]:
+                file_name = "data/" + str(movie["id"]) + ".json"
+                with open(file_name, "r") as file:
+                    movie_json = json.load(file)
+                    word_count = len(movie_json[field].split())
+                    print(str(word_count) + ": " + str(movie["title"]) + " (" + str(movie["id"]) + ")")
+
+# RUN CODE HERE
+
+# debug_wordcount("richtext")
+
+# initialize_movie_richtext()
+
+
+
+    
+
+# OLDER STUFF BELOW HERE
+
+# claude poc
+def hello_claude():
+    client = Anthropic(
+        CLAUDE_KEY #api_key=os.environ.get("ANTHROPIC_API_KEY")
+    )
+
+    message = client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": "Hello, Claude",
+            }
+        ],
+        model="claude-opus-4-6",
+    )
+    print(message.content)
---
+++ init_embeddings.py
@@ -0,0 +1,88 @@
+import json
+import glob
+import chromadb
+import sys
+
+from sentence_transformers import SentenceTransformer
+
+# load model (downloads automatically)
+MODEL = SentenceTransformer('all-mpnet-base-v2')
+
+# load chromadb
+chroma = chromadb.PersistentClient(path='./embeddings/chroma_db')
+collection = chroma.get_or_create_collection(
+    name='movies',
+    metadata={'hnsw:space': 'cosine'}  # cosine similarity
+)
+
+# embed a single string
+def embed_text_single(text: str) -> list[float]:
+    vector = MODEL.encode(text, normalize_embeddings=True)
+    return vector.tolist()
+
+# upsert one movie
+def upsert_movie_single(id: str):
+    file = "data/" + id + ".json"
+    with open(file, "r") as f:
+        movie = json.load(f)
+        richtext = movie["richtext"]
+        embeddings = MODEL.encode(richtext)
+        collection.upsert(ids=[id],
+                        embeddings=[embeddings],
+                        documents=[richtext])
+
+    #print(collection.count())
+    #print(collection.peek())
+
+# load all richtext strings into memory for batch embedding
+def load_richtexts_batch() -> tuple[list[str], list[str]]:
+    ids, texts = [], []
+    for file in glob.glob(f"./data/*.json"):
+        if("index" not in file):
+            with open(file) as f:
+                movie = json.load(f)
+            ids.append(str(movie["id"]))
+            texts.append(movie["richtext"])
+    return ids, texts # returns tuple
+
+# batch embeddings
+def get_embeddings_batch(texts: list[str], batch_size: int = 64) -> list[list[float]]:
+    vectors = MODEL.encode(texts, batch_size=batch_size,
+                           normalize_embeddings=True,
+                           show_progress_bar=True)
+    # MODEL.encode() returns NumPy ndarray
+    return vectors.tolist()
+
+def initialize_embeddings_batch():
+    richtexts = load_richtexts_batch()
+    ids = richtexts[0]
+    docs = richtexts[1]
+
+    # Embed in batches of 64
+    embeddings = get_embeddings_batch(docs, batch_size=64)
+
+    # Upsert into Chroma (safe to re-run)
+    collection.upsert(ids=ids, embeddings=embeddings,
+                      documents=docs)
+    print(f"Ingested {len(ids)} movies into Chroma.")
+    #print(f"\n\n{collection.count()}")
+    #print(f"\n\n{collection.peek()}")
+
+    
+# 
+#initialize_embeddings_batch()
+
+def search(query):
+    q_embeddings = MODEL.encode(query)
+    results = collection.query(query_embeddings=q_embeddings)
+    
+    print(f"\n\nFound {len(results)} matches for {query}\n")
+    for match in results["documents"]:
+        for movie in match:
+            title = movie.split('\n', maxsplit=1)[0]
+            print(f"{title}")
+
+
+# > python3 init_embeddings.py search <query>
+search(sys.argv[1:])
+
